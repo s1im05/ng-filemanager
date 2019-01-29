893eec2c49ca43787b7451a9aef0bf32dee8f82e
Vadim Suleimanov <s1im05@mail.ru>
2019-01-29 13:07:36 +0100

    Save toggle switcher state

--- src/app/layout/nav-top/nav-top.component.ts
+++ src/app/layout/nav-top/nav-top.component.ts
@@ -7,13 +7,25 @@ import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 })
 export class NavTopComponent implements OnInit {
 
-    @Input() showHidden = true;
+    @Input() showHidden;
     @Output() hiddenToggle = new EventEmitter();
+    showHiddenKey = 'showHidden';
 
     constructor() {
     }
 
     ngOnInit() {
+        this.showHidden = window.localStorage.getItem(this.showHiddenKey) !== null &&
+            window.localStorage.getItem(this.showHiddenKey) === '1';
+
+        window.setTimeout(() => {
+            this.hiddenToggle.emit(this.showHidden);
+        });
+    }
+
+    onHiddenToggle() {
+        this.hiddenToggle.emit(this.showHidden = !this.showHidden);
+        window.localStorage.setItem(this.showHiddenKey, this.showHidden ? '1' : '0');
     }
 
 }
